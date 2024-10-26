2f7ca11d70a9ebfd2e05b53d79ecaa8071bc6a5d
joisemp <joise1124@gmail.com>
2024-10-26 22:44:50 +0530

    Created login view
    - Added new settings only for tests
    - Created script in script directory for runing script

---
+++ script/run_test.bat
@@ -0,0 +1,3 @@
+cd src
+set DJANGO_SETTINGS_MODULE=config.test_settings
+python manage.py test
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from . forms import CustomAuthenticationForm
 
-# Create your views here.
+
+class LoginView(LoginView):
+    form_class = CustomAuthenticationForm
+    template_name = 'core/login.html'
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return redirect('landing_page')
