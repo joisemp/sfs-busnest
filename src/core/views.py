9ef67b05106edfd3e0347c3df9b0e7212658e54e
joisemp <joise1124@gmail.com>
2024-10-29 10:30:44 +0530

    Created register view

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
-from . forms import CustomAuthenticationForm
+from django.urls import reverse_lazy
+from . forms import CustomAuthenticationForm, UserRegisterForm
+from django.views.generic import CreateView, View
+from core.models import UserProfile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginView(LoginView):
@@ -11,3 +17,27 @@ class LoginView(LoginView):
     def form_valid(self, form):
         login(self.request, form.get_user())
         return redirect('landing_page')
+    
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'core/register.html'
+    success_url = reverse_lazy('core:login')
+    
+    def form_valid(self, form):
+        user = form.save()
+        
+        # Create user profile
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        UserProfile.objects.create(
+            user=user, 
+            first_name=first_name, 
+            last_name=last_name, 
+            is_central_admin=True
+            )
+
+        login(self.request, user)
+        return redirect('landing_page')
+    
