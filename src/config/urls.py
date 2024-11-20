9044087dd1424bbd6980d7efa1318fe5e461e279
joisemp <joise1124@gmail.com>
2024-11-20 14:52:55 +0530

    Configured student urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('core/', include('core.urls', namespace='core')),
     path('central_admin/', include('services.urls.central_admin', namespace='central_admin')),
     path('institution_admin/', include('services.urls.institution_admin', namespace='institution_admin')),
+    path('students/', include('services.urls.students', namespace='students')),
 ]
+
---
+++ src/services/urls/students.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from services.views import students
+
+app_name = 'students'
+
+
+urlpatterns = [
+     # path('', institution_admin.InstitutionListView.as_view(), name='institution_detail'),
+]
