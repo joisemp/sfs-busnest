af554bc5fd2e4a75febbdcc91d37cbef80745b06
joisemp <joise1124@gmail.com>
2024-11-26 22:42:12 +0530

    Created student group list view

--- src/services/urls/institution_admin.py
+++ src/services/urls/institution_admin.py
@@ -7,4 +7,5 @@ app_name = 'institution_admin'
 urlpatterns = [
      path('registrations/', institution_admin.RegistrationListView.as_view(), name='registration_list'),
      path('recipts/', institution_admin.ReciptListView.as_view(), name='recipt_list'),
+     path('student-groups/', institution_admin.StudentGroupListView.as_view(), name='student_group_list'),
 ]
--- src/services/views/institution_admin.py
+++ src/services/views/institution_admin.py
@@ -1,6 +1,6 @@
 from django.views.generic import FormView, ListView, CreateView
 from django.urls import reverse
-from services.models import Registration, Recipt
+from services.models import Registration, Recipt, StudentGroup
 
 class RegistrationListView(ListView):
     model = Registration
@@ -12,5 +12,11 @@ class ReciptListView(ListView):
     model = Recipt
     template_name = 'institution_admin/recipt_list.html'
     context_object_name = 'recipts'
+    
+    
+class StudentGroupListView(ListView):
+    model = StudentGroup
+    template_name = 'institution_admin/student_group_list.html'
+    context_object_name = 'student_groups'   
     
     
