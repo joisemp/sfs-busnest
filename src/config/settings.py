48c529ad9be4e02c99a3b9cc905a27197b5f8da7
joisemp <joise1124@gmail.com>
2024-12-27 20:44:54 +0530

    Added postgres to docker compose and made changes to the settings to connect to it

--- src/config/settings.py
+++ src/config/settings.py
@@ -67,10 +67,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Development Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
