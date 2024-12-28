78fa295c43b46da10c8316fa6bea0d2f966c50c5
joisemp <joise1124@gmail.com>
2024-12-28 14:57:19 +0530

    Added functionlity to notify user on addition to the system

--- src/services/tasks.py
+++ src/services/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 import time
 import logging
+from django.core.mail import send_mail
 
 logger = logging.getLogger(__name__)
 
@@ -21,3 +22,20 @@ def send_newsletter():
         time.sleep(1)
     logger.info("All Newsletter sent!!")
     return 'COMPLETED!!!!!'
+
+@shared_task(name='send_email_task')
+def send_email_task(subject, message, recipient_list, from_email=None):
+    try:
+        logger.info(f"Starting email task: Sending email to {recipient_list}")
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            fail_silently=False,
+        )
+        logger.info(f"Email successfully sent to {recipient_list}")
+        return "Email sent successfully"
+    except Exception as e:
+        logger.error(f"Failed to send email to {recipient_list}: {e}")
+        raise
