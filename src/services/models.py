bc086fa7a861571339195dfc5cf8af7c77021322
joisemp <joise1124@gmail.com>
2025-02-14 23:47:02 +0530

    Updated few templates

--- src/services/models.py
+++ src/services/models.py
@@ -237,6 +237,9 @@ class Trip(models.Model):
         if not self.record or not self.record.bus or self.record.bus.capacity == 0:
             return 0
         return round((self.booking_count * 100) / self.record.bus.capacity, 2)
+
+    def get_total_filled_seats_percentage(self):
+        return self.total_filled_seats_percentage
     
     def __str__(self):
         return f"{self.schedule} | {self.route}"
