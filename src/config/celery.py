dc927f65326e4771c006189a9b0fb1517c7efa1e
joisemp <joise1124@gmail.com>
2025-05-25 22:56:37 +0530

    Documented few files

--- src/config/celery.py
+++ src/config/celery.py
@@ -1,4 +1,16 @@
 import os
+"""
+This module configures the Celery application for the Django project.
+The Celery application is initialized with the following steps:
+1. Sets the default Django settings module to 'config.settings'.
+2. Creates a Celery application instance named 'config'.
+3. Configures the Celery application using Django's settings, with the namespace 'CELERY'.
+4. Automatically discovers tasks from all installed Django apps.
+Attributes:
+    celery_app (Celery): The Celery application instance used for task management.
+Usage:
+    Import `celery_app` in other modules to define or execute Celery tasks.
+"""
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
--- src/config/urls.py
+++ src/config/urls.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from django.urls import path, include
 from config.views import LandingPageView, count_to_10
 
+"""
+URL configuration for the BMS-SFS Institutions project.
+This module defines the URL patterns for the project, mapping URLs to their
+corresponding views or including other URL configurations.
+Routes:
+- 'admin/': Admin site URLs.
+- '': Landing page view.
+- 'count/': View to count to 10.
+- 'core/': Includes the URL patterns from the 'core' app.
+- 'central_admin/': Includes the URL patterns for central administration services.
+- 'institution_admin/': Includes the URL patterns for institution administration services.
+- 'students/': Includes the URL patterns for student-related services.
+Namespaces:
+- 'core': Namespace for the 'core' app.
+- 'central_admin': Namespace for central administration services.
+- 'institution_admin': Namespace for institution administration services.
+- 'students': Namespace for student-related services.
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing_page'),
